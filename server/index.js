80a257255e8f34c3669b322d7386b5683bff736d
Richard Pos <richardnottoknow@gmail.com>
2019-09-19 14:59:46 +0200

    Passport authentication with google implemeted

--- server/index.js
+++ server/index.js
@@ -1,20 +1,34 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-/*
-app           get         '/'           req             res             res.send({ hi: 'there'})
- |             |           |             |               |                          |
-Express     Watch for     watch for     object          object            immediately send
-App to      incoming      requests      representing    representing      some JSON back to
-register    requests      trying to     the incoming    the outgoing      who ever made
-this        with this     access        request         respons           this request
-route       method        '/'
-handler 
-with
-*/
-app.get('/', (req, res) => {
-  res.send({ bey: 'buddy' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token: --> ', accessToken);
+      console.log('refreshToken: --> ', refreshToken);
+      console.log('profile: --> ', profile);
+      // done(); localhost:5000/auth/google
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
