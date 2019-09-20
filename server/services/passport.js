685d53cd87fa6c721c600addeac123efb70b104f
Richard Pos <richardnottoknow@gmail.com>
2019-09-20 15:17:35 +0200

    Finished Section 4 untill part 47

--- server/services/passport.js
+++ server/services/passport.js
@@ -5,6 +5,22 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+// for the explanation of this see explanation passport serializeUser method.png
+passport.serializeUser((user, done) => {
+  // user.id is the database id of logged in user see 
+  // Explanation using our own internal id in cookies.png
+  done(null, user.id);
+});
+
+// id is the user.id from serializeUser above
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+})
+
+// for the flow of passport see Explanation passportjs flow.png
 passport.use(
   new GoogleStrategy(
     {
@@ -13,9 +29,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save;
-      done();
-      // done(); localhost:5000/auth/google
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            // We already have a user record with this profile.id
+            done(null, existingUser);
+          } else {
+            // We don't have a user record with this profile.id
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        });
     }
   )
 );
