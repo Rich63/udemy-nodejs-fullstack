3ee16922c895065cd91113d5f689ba975114ece5
Richard Pos <richardnottoknow@gmail.com>
2019-09-25 13:57:43 +0200

    Finished Section 8 untill part 113

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -7,14 +7,8 @@ export const fetchUser = () => async dispatch => {
   dispatch({ type: FETCH_USER, payload: res.data })
 }
 
-// above code is the same as below code
-// export const fetchUser = () => { 
-//   return function(dispatch) {
-//     axios
-//       .get('/api/current_user')
-//       .then(res => dispatch({
-//         type: FETCH_USER, 
-//         payload: res
-//       }))
-//   }
-// }
+export const handleToken = (token) => async dispatch => {
+  const res = await axios.post('/api/stripe', token)
+
+  dispatch({ type: FETCH_USER, payload: res.data })
+}
--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import Payments from './Payments'
+
 class Header extends React.Component {
   renderContent() {
     switch (this.props.auth) {
@@ -10,7 +12,13 @@ class Header extends React.Component {
       case false:
         return <li><a href='/auth/google'>Login with Google</a></li>
       default:
-        return <li><a href='/api/logout'>Logout</a></li>
+        return [
+          <li key='1'><Payments /></li>,
+          <li key='2' style={{ margin: '0 10px' }}>
+            Credits: { this.props.auth.credits }
+          </li>,
+          <li key='3'><a href='/api/logout'>Logout</a></li>
+        ]
     }
   }
 
